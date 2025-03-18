be93cf77e943353af45d9fbdec0072525a97a493
danzo-git <kouamedenzel@gmail.com>
2025-03-18 07:33:52 +0100

    resolution sous-categorie

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,23 +2,43 @@
 
 namespace App\Controller;
 
+use App\Repository\CategoryRepository;
 use App\Repository\ProductRepository;
-
 use App\Repository\SubCategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
- class HomeController extends AbstractController
+class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(ProductRepository $productRepository): Response
-    {
+    public function index(
+        ProductRepository $productRepository,
+        CategoryRepository $categoryRepository,
+        SubCategoryRepository $subCategoryRepository
+    ): Response {
+        // Get categories with random products
+        $productsByRandom = $categoryRepository->findBy([], ['name' => 'ASC']);
+        
+        // Get all products
+        $products = $productRepository->findAll();
+        
+        // Get new products (you can modify this criteria as needed)
+        $newProducts = $productRepository->findBy([], ['id' => 'DESC'], 10);
+        
+        // Get top products/sales (you can modify this criteria as needed)
+        // This is just a placeholder, replace with your actual logic
+        $topProducts = $productRepository->findBy([], ['id' => 'ASC'], 10);
+        
+        // Get all subcategories for the filter tabs
+        $subCategories = $subCategoryRepository->findAll();
+        
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-            'products' => $productRepository->find10Biens($productRepository->getEntityManager()),
-            'newProducts' => $productRepository->findBy([], ['id' => 'DESC'], 10),
-            'productsByRandom' => $productRepository->find3ProductsByRandomWithSubCategoryAndCategoryAndImagesAssociated($productRepository->getEntityManager()),
+            'productsByRandom' => $productsByRandom,
+            'products' => $products,
+            'newProducts' => $newProducts,
+            'topProducts' => $topProducts,
+            'subCategories' => $subCategories,
         ]);
     }
 }
